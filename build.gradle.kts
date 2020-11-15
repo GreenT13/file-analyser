5f04625b607ab1960b3844c00010266aa4273f35
Rico Apon <rico.apon@hotmail.com>
2020-11-15 10:46:38 +0100

    #3 Add SpotBugs
    SpotBugs is a tool to check for common bugs in the code. This will help
    keep the quality of the code high. It is automatically run when gradle
    check is run.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    id("com.github.spotbugs") version "4.5.0"
 }
 
 java {
@@ -14,6 +15,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.github.spotbugs:spotbugs-annotations:4.1.1")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
     testImplementation("org.hamcrest:hamcrest:2.2")
@@ -22,4 +25,28 @@ dependencies {
 val test by tasks.getting(Test::class) {
     // Use junit platform for unit tests
     useJUnitPlatform()
+}
+
+// ================
+// SpotBugs
+// ================
+spotbugs {
+    // Display final report as HTML.
+    // Use different HTML template (stylesheet) that is prettier.
+    tasks.spotbugsMain {
+        reports.create("html") {
+            isEnabled = true
+            setStylesheet("fancy-hist.xsl")
+        }
+    }
+    tasks.spotbugsTest {
+        reports.create("html") {
+            isEnabled = true
+            setStylesheet("fancy-hist.xsl")
+        }
+    }
+}
+tasks.register("spotbugs") {
+    dependsOn(tasks.spotbugsMain)
+    dependsOn(tasks.spotbugsTest)
 }
